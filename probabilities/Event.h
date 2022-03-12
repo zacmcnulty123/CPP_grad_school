0de9139303abc8ceac4d104a441705ec6e4014be
zacmcnulty123 <zacharymcnulty123@outlook.com>
2022-03-12 14:51:59 -0500

    Code draft done

--- probabilities/Event.h
+++ probabilities/Event.h
@@ -1,80 +1,133 @@
 #pragma once
 #include<iostream>
+#include<sstream>
 class Event
 {
 private:
-  double likelihood;
+  double prob;
 public:
-  Event(/* args */) : likelihood(0.0) {}
-  Event(double likelihood) {
-    if (likelihood > 1.0 or likelihood < 0.0) {
-      throw std::invalid_argument("Likelihood must be between" 
+  Event(/* args */) : prob(0.0) {}
+  Event(double prob) {
+    if (prob > 1.0 or prob < 0.0) {
+      throw std::invalid_argument("probability must be between" 
         "0.0 and 1.0");
     }
-    this->likelihood = likelihood;
+    this->prob = prob;
   }
 
   ~Event() {}
   
-  double getLikelihoodOfOccuring() const {
-    return this->likelihood;
+  double getProb() const {
+    return this->prob;
   }
 
-  double getLikelihoodOfNotOccuring() const {
-    return 1.0 - this->likelihood;
+  double getNotProb() const {
+    return 1.0 - this->prob;
   }
 
-  void setLikelihood(double likelihood) {
-    if (likelihood > 1.0 or likelihood < 0.0) {
-      throw std::invalid_argument("Likelihood must be between" 
+  void setProb(double prob) {
+    if (prob > 1.0 or prob < 0.0) {
+      throw std::invalid_argument("probability must be between" 
         "0.0 and 1.0");
     }
-    this->likelihood = likelihood;
+    this->prob = prob;
   }
 
-  double getAandBLikelihood(const Event & B) const {
-    //P(A and B) = P(A) * P(B|A) or P(B) * P(A | B)
-    //A and B are given in this case so the probabilities reduce
-    //to their independent probabilities
-    //P(A and B) = P(A) * P(B) or P(B) * P(A)
-    return (this->likelihood * B.getLikelihoodOfOccuring());
+  std::string toString() const {
+    std::stringstream ss;
+    ss << "P = " << this->prob << " ~P = " << this->getNotProb();
+    return ss.str();
   }
 
-  double getAorBLikelihood(const Event & B) const {
-    //P(A or B) = P(A) + P(B) - P(A and B)
-    return (this->likelihood + B.getLikelihoodOfOccuring() - getAandBLikelihood(B));
-  }
-
-  double getAnorBLikelihood(const Event & B) const {
-    //P(~A and ~B) = P(~A) * P(~B | ~A) ~A is given so reduces to P(~B)
-    //P(~A and ~B) = P(~A) * P(~B)
-    return (this->getLikelihoodOfNotOccuring() * B.getLikelihoodOfNotOccuring());
-  }
-
-  double getANotBLikelihood(const Event & B) const {
-    //A - B is the probability of A but not B occurring
-    //P(A and ~B) = P(A - B)
-    //P(A) - P(A and B)
-    //P(A) - P(A)P(B)
-    //P(A) * (1 - P(B))
-    //P(A) * P(~B)
-    return (this->likelihood * B.getLikelihoodOfNotOccuring());
-  }
-
-  double operator &(const Event & B) {
-    this->likelihood += 0.1;
-    return (this->getAandBLikelihood(B));
-  }
-
-  double operator |(const Event & B) {
-    return (this->getAorBLikelihood(B));
-  }
-
-  double operator ^(const Event & B) {
-    return (this->getAnorBLikelihood(B));
-  }
-
-  double operator -(const Event & B) {
-    return (this->getANotBLikelihood(B));
-  }
 };
+
+Event operator ~(const Event & A) {
+  Event ret(A.getNotProb());
+  return ret;
+}
+
+double operator &(const Event &A, const Event & B) {
+  //P(A and B) = P(A) * P(B|A) or P(B) * P(A | B)
+  //A and B are given in this case so the probabilities reduce
+  //to their independent probabilities
+  //P(A and B) = P(A) * P(B) or P(B) * P(A)
+  return (A.getProb() * B.getProb());
+}
+
+double operator &=(const Event & A, const Event & B) {
+  return (A & B);
+}
+
+double operator |(const Event & A, const Event & B) {
+  //P(A or B) = P(A) + P(B) - P(A and B)
+  return (A.getProb() + B.getProb() - (A & B));
+}
+
+double operator |=(const Event & A, const Event & B) {
+  return (A | B);
+}
+
+double operator ^(const Event & A, const Event & B) {
+  //P(~A and ~B) = P(~A) * P(~B | ~A) ~A is given so reduces to P(~B)
+  //P(~A and ~B) = P(~A) * P(~B)
+  return (A.getNotProb() + B.getNotProb() - (~A & ~B));
+}
+
+double operator ^=(const Event & A, const Event & B) {
+  return (A ^ B);
+}
+
+double operator -(const Event & A, const Event & B) {
+  //A - B is the probability of A but not B occurring
+  //P(A and ~B) = P(A - B)
+  //P(A) - P(A and B)
+  //P(A) - P(A)P(B)
+  //P(A) * (1 - P(B))
+  //P(A) * P(~B)
+  return (A & ~B);
+}
+
+double operator -=(const Event & A, const Event & B) {
+  return (A - B);
+}
+
+bool operator ==(const Event & A, const Event & B) {
+  return (A.getProb() == B.getProb());
+}
+
+bool operator <(const Event & A, const Event & B) {
+  return (A.getProb() < B.getProb());
+}
+
+bool operator >(const Event & A, const Event & B) {
+  return (A.getProb() > B.getProb());
+}
+
+bool operator !=(const Event & A, const Event & B) {
+  return (A.getProb() != B.getProb());
+}
+
+bool operator >=(const Event & A, const Event & B) {
+  if (A == B)
+  {
+    return true;
+  }
+  else if (A > B) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+bool operator <=(const Event & A, const Event & B) {
+  if (A == B) {
+    return true;
+  }
+  else if (A < B) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
--- probabilities/main.cpp
+++ probabilities/main.cpp
@@ -1,12 +1,24 @@
 #include"Event.h"
-
+#include<vector>
+#include<iomanip>
 using namespace std;
+
 int main(void) {
-  Event A = Event(0.5);
-  Event B = Event(0.5);
-  cout << (A & B) << endl;
-  cout << (A | B) << endl;
-  cout << (A ^ B) << endl;
-  cout << (A - B) << endl;
+  std::vector<Event> A = {0.0, 0.25, 0.5, 0.75, 1.0, 0.3, 0.8, 0.45, 0.16};
+  std::vector<Event> B = {0.0, 0.25, 0.5, 0.75, 1.0, 0.6, 0.12, 0.98, 0.33};
+  for (int i = 0; i < A.size(); i++) {
+    cout << "Testing Event # " << i << endl;
+    cout << "P(A): " << A[i].toString() << endl;
+    cout << "P(B): " << B[i].toString() << endl; 
+    cout << "P(A and B)" << " | " << setw(5) 
+        << "P(A or B)" << " | " << setw(5)
+      << "P(~A and ~B)" << " | " << setw(5)
+      << "P(A and ~B)" << endl;
+    cout << (A[i] & B[i]) << setw(15);
+    cout << (A[i] | B[i]) << setw(15);
+    cout << (A[i] ^ B[i])  << setw(15);
+    cout << (A[i] - B[i])  << setw(15);
+    cout << endl;
+  }
   return 0;
 }
